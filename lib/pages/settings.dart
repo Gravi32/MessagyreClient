c5f66b400bd74e6e4784f7d127a6d5ac8620685d
Gravi32 <gravina.pietro@gmail.com>
2025-06-22 21:59:34 +0200

    Moved the settings subpages in separate files for better organization

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:messagyre_client/pages/overlays/profile.dart';
+import 'package:messagyre_client/pages/settings_subpages/debug_settings.dart';
+import 'package:messagyre_client/pages/settings_subpages/feedback_settings.dart';
+import 'package:messagyre_client/pages/settings_subpages/storage_settings.dart';
 import 'package:messagyre_client/singletons/connection_controller.dart';
 import 'package:messagyre_client/singletons/data.dart';
 import 'package:messagyre_client/utility/classes.dart';
@@ -23,7 +26,6 @@ class _SettingsPageState extends State<SettingsPage> {
   final secureStorage = FlutterSecureStorage();
 
   late bool isDarkMode;
-  bool isRefreshTokenStored = false;
 
   Account? account;
 
@@ -66,171 +68,11 @@ class _SettingsPageState extends State<SettingsPage> {
     );
   }
 
-  Future<void> checkRefreshToken() async {
-    final token = await secureStorage.read(key: "RefreshToken");
-    setState(() {
-      isRefreshTokenStored = token != null;
-    });
-  }
-
-  Widget storagePage() {
-    void confirmDeleteChats() {
-      showCupertinoDialog(
-        context: context,
-        builder:
-            (dialogContext) => CupertinoAlertDialog(
-              title: Text("Effacer les conversations"),
-              content: Text(
-                "Toutes les conversations seront effacées, cette action est irréversible.",
-              ),
-              actions: [
-                CupertinoDialogAction(
-                  onPressed: () => Navigator.of(dialogContext).pop(),
-                  child: Text("Annuler"),
-                ),
-                CupertinoDialogAction(
-                  isDestructiveAction: true,
-                  onPressed: () async {
-                    try {
-                      if (Hive.isBoxOpen("Chats")) {
-                        await Hive.box<Chat>("Chats").clear();
-                      } else if (await Hive.boxExists("Chats")) {
-                        var box = await Hive.openBox<Chat>("Chats");
-                        await box.clear();
-                      }
-                    } catch (e, s) {
-                      debugPrintStack(stackTrace: s, label: e.toString());
-                    }
-
-                    if (dialogContext.mounted) {
-                      Navigator.of(dialogContext).pop();
-                    }
-                  },
-                  child: Text("Effacer"),
-                ),
-              ],
-            ),
-      );
-    }
-
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        previousPageTitle: "Réglages",
-        middle: Text("Effacer les données"),
-      ),
-      child: SafeArea(
-        child: SettingsList(
-          platform: DevicePlatform.iOS,
-          sections: [
-            SettingsSection(
-              tiles: [
-                SettingsTile(
-                  title: Text(
-                    "Effacer les conversations",
-                    style: TextStyle(color: CupertinoColors.destructiveRed),
-                  ),
-                  onPressed: (context) {
-                    confirmDeleteChats();
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget debugPage() {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        previousPageTitle: "Réglages",
-        middle: Text("Débogage"),
-      ),
-      child: SafeArea(
-        child: SettingsList(
-          platform: DevicePlatform.iOS,
-          sections: [
-            SettingsSection(
-              title: Text("Informations générales"),
-              tiles: [
-                SettingsTile(
-                  title: Text("Nom d'utilisateur"),
-                  value: Text(data.username ?? "-"),
-                ),
-                SettingsTile(
-                  title: Text("Photos de profil en cache"),
-                  value: Text(data.pfpNotifiersCache.length.toString()),
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: Text("Connexion"),
-              tiles: [
-                SettingsTile(
-                  title: Text("Mode de test local"),
-                  value: Text(
-                    ConnectionController.useLocalhost ? "Oui" : "Non",
-                  ),
-                ),
-
-                SettingsTile(
-                  title: Text("Adresse du serveur"),
-                  value: Text(
-                    ConnectionController.serverHTTPAddress.substring(7),
-                  ),
-                ),
-
-                SettingsTile(
-                  title: Text("Dernière requête HTTP"),
-                  value: Text(data.token != null ? "Oui" : "Non"),
-                ),
-
-                SettingsTile(
-                  title: Text("État du WebSocket"),
-                  value: ValueListenableBuilder<bool>(
-                    valueListenable: data.isConnecting,
-                    builder: (context, isConnecting, _) {
-                      if (isConnecting) {
-                        return Text("Connexion en cours...");
-                      }
-
-                      return ValueListenableBuilder<bool>(
-                        valueListenable: data.isConnected,
-                        builder: (context, isConnected, _) {
-                          return Text(isConnected ? "Connecté" : "Déconnecté");
-                        },
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: Text("Tokens"),
-              tiles: [
-                SettingsTile(
-                  title: Text("Token d'accès (JWT)"),
-                  value: Text(data.token != null ? "Oui" : "Non"),
-                ),
-                SettingsTile(
-                  title: Text("Token de renouvellement"),
-                  value: Text(isRefreshTokenStored ? "Oui" : "Non"),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
   @override
   void initState() {
     super.initState();
     isDarkMode = data.appBrightness == Brightness.dark;
     getAccount();
-    checkRefreshToken();
   }
 
   @override
@@ -335,7 +177,9 @@ class _SettingsPageState extends State<SettingsPage> {
                 SettingsTile.navigation(
                   onPressed:
                       (context) => Navigator.of(context).push(
-                        CupertinoPageRoute(builder: (context) => storagePage()),
+                        CupertinoPageRoute(
+                          builder: (context) => StorageSettingsPage(),
+                        ),
                       ),
                   leading: Icon(CupertinoIcons.delete),
                   title: Text("Effacer les données"),
@@ -349,7 +193,9 @@ class _SettingsPageState extends State<SettingsPage> {
                 SettingsTile.navigation(
                   onPressed:
                       (context) => Navigator.of(context).push(
-                        CupertinoPageRoute(builder: (context) => debugPage()),
+                        CupertinoPageRoute(
+                          builder: (context) => FeedbackSettingsPage(),
+                        ),
                       ),
                   leading: Icon(CupertinoIcons.bubble_right),
                   title: Text("Envoyez un commentaire"),
@@ -357,7 +203,9 @@ class _SettingsPageState extends State<SettingsPage> {
                 SettingsTile.navigation(
                   onPressed:
                       (context) => Navigator.of(context).push(
-                        CupertinoPageRoute(builder: (context) => debugPage()),
+                        CupertinoPageRoute(
+                          builder: (context) => DebugSettingsPage(),
+                        ),
                       ),
                   leading: Icon(CupertinoIcons.ant),
                   title: Text("Débogage"),
---
+++ lib/pages/settings_subpages/debug_settings.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:messagyre_client/singletons/connection_controller.dart';
+import 'package:messagyre_client/singletons/data.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+class DebugSettingsPage extends StatefulWidget {
+  const DebugSettingsPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _DebugSettingsPageState();
+}
+
+class _DebugSettingsPageState extends State<DebugSettingsPage> {
+  final data = Data();
+  final secureStorage = FlutterSecureStorage();
+
+  bool isRefreshTokenStored = false;
+
+  Future<void> checkRefreshToken() async {
+    final token = await secureStorage.read(key: "RefreshToken");
+    setState(() {
+      isRefreshTokenStored = token != null;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    checkRefreshToken();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        previousPageTitle: "Réglages",
+        middle: Text("Débogage"),
+      ),
+      child: SafeArea(
+        child: SettingsList(
+          platform: DevicePlatform.iOS,
+          sections: [
+            SettingsSection(
+              title: Text("Informations générales"),
+              tiles: [
+                SettingsTile(
+                  title: Text("Nom d'utilisateur"),
+                  value: Text(data.username ?? "-"),
+                ),
+                SettingsTile(
+                  title: Text("Photos de profil en cache"),
+                  value: Text(data.pfpNotifiersCache.length.toString()),
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: Text("Connexion"),
+              tiles: [
+                SettingsTile(
+                  title: Text("Mode de test local"),
+                  value: Text(
+                    ConnectionController.useLocalhost ? "Oui" : "Non",
+                  ),
+                ),
+
+                SettingsTile(
+                  title: Text("Adresse du serveur"),
+                  value: Text(
+                    ConnectionController.serverHTTPAddress.substring(7),
+                  ),
+                ),
+
+                SettingsTile(
+                  title: Text("Dernière requête HTTP"),
+                  value: Text(data.token != null ? "Oui" : "Non"),
+                ),
+
+                SettingsTile(
+                  title: Text("État du WebSocket"),
+                  value: ValueListenableBuilder<bool>(
+                    valueListenable: data.isConnecting,
+                    builder: (context, isConnecting, _) {
+                      if (isConnecting) {
+                        return Text("Connexion en cours...");
+                      }
+
+                      return ValueListenableBuilder<bool>(
+                        valueListenable: data.isConnected,
+                        builder: (context, isConnected, _) {
+                          return Text(isConnected ? "Connecté" : "Déconnecté");
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: Text("Tokens"),
+              tiles: [
+                SettingsTile(
+                  title: Text("Token d'accès (JWT)"),
+                  value: Text(data.token != null ? "Oui" : "Non"),
+                ),
+                SettingsTile(
+                  title: Text("Token de renouvellement"),
+                  value: Text(isRefreshTokenStored ? "Oui" : "Non"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
