91797b4544a9292f0480ecdbc44e8bcb51ad3c4f
Gravi32 <gravina.pietro@gmail.com>
2025-06-27 13:31:39 +0200

    Added the grade page placeholder

--- lib/pages/grades.dart
+++ lib/pages/grades.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:messagyre_client/singletons/connection_controller.dart';
+import 'package:messagyre_client/singletons/data.dart';
 
 class GradesPage extends StatefulWidget {
   const GradesPage({super.key});
@@ -8,12 +10,47 @@ class GradesPage extends StatefulWidget {
 }
 
 class _GradesPageState extends State<GradesPage> {
+  final router = ConnectionController();
+  final data = Data();
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoTabView(
-      builder: (context) {
-        return Text("asd");
-      },
+    return CupertinoPageScaffold(
+      child: Stack(
+        children: [
+          NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [
+                CupertinoSliverNavigationBar(largeTitle: Text("Notes")),
+              ];
+            },
+            body: SafeArea(
+              top: false,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 10,
+                  children: [
+                    Icon(
+                      CupertinoIcons.sparkles,
+                      size: 40,
+                      color: CupertinoColors.separator.resolveFrom(context),
+                    ),
+                    Text(
+                      "Ajoutez une note...",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: CupertinoColors.separator.resolveFrom(context),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
