d375d9163a3696089a0c80380693c70f1348168c
Gravi32 <gravina.pietro@gmail.com>
2025-09-01 20:19:04 +0200

    Made it so that you can't go back from the login page by using android "back" button/swipe

--- lib/access.dart
+++ lib/access.dart
@@ -99,163 +99,173 @@ class _AccessOverlayState extends State<AccessOverlay> {
     final fieldsAreEmpty =
         usernameController.text.isEmpty || passwordController.text.isEmpty;
 
-    return CupertinoPageScaffold(
-      child: SafeArea(
-        minimum: const EdgeInsets.symmetric(horizontal: 20),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(minHeight: constraints.maxHeight),
-                child: IntrinsicHeight(
-                  child: ValueListenableBuilder(
-                    valueListenable: isWaitingForResponse,
-                    builder:
-                        (context, isDisabled, _) => Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Image.asset(
-                              "assets/icons/logo_purple.png",
-                              height: 100,
-                            ),
+    return PopScope(
+      canPop: false,
+      child: CupertinoPageScaffold(
+        child: SafeArea(
+          minimum: const EdgeInsets.symmetric(horizontal: 20),
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                  child: IntrinsicHeight(
+                    child: ValueListenableBuilder(
+                      valueListenable: isWaitingForResponse,
+                      builder:
+                          (context, isDisabled, _) => Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Image.asset(
+                                "assets/icons/logo_purple.png",
+                                height: 100,
+                              ),
 
-                            const SizedBox(height: 10),
+                              const SizedBox(height: 10),
 
-                            Text(
-                              "Bienvenue sur Messagyre",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.w600,
-                                color: CupertinoColors.label.resolveFrom(
-                                  context,
+                              Text(
+                                "Bienvenue sur Messagyre",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w600,
+                                  color: CupertinoColors.label.resolveFrom(
+                                    context,
+                                  ),
                                 ),
                               ),
-                            ),
 
-                            const SizedBox(height: 6),
+                              const SizedBox(height: 6),
 
-                            Text(
-                              "connectez-vous pour continuer",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 15,
-                                color: CupertinoColors.label
-                                    .resolveFrom(context)
-                                    .withOpacity(.7),
+                              Text(
+                                "connectez-vous pour continuer",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: CupertinoColors.label
+                                      .resolveFrom(context)
+                                      .withOpacity(.7),
+                                ),
                               ),
-                            ),
 
-                            const SizedBox(height: 40),
+                              const SizedBox(height: 40),
 
-                            CustomTextField(
-                              title: "Nom d'utilisateur",
-                              placeholder: "prénom.nom",
-                              error: usernameError,
-                              controller: usernameController,
-                              disabled: isDisabled,
-                              onChanged:
-                                  (_) => setState(() {
-                                    usernameController.text = usernameController
-                                        .text
-                                        .toLowerCase()
-                                        .replaceAll(' ', '');
-                                  }),
-                            ),
+                              CustomTextField(
+                                title: "Nom d'utilisateur",
+                                placeholder: "prénom.nom",
+                                error: usernameError,
+                                controller: usernameController,
+                                disabled: isDisabled,
+                                onChanged:
+                                    (_) => setState(() {
+                                      usernameController
+                                          .text = usernameController.text
+                                          .toLowerCase()
+                                          .replaceAll(' ', '');
+                                    }),
+                              ),
 
-                            const SizedBox(height: 20),
+                              const SizedBox(height: 20),
 
-                            CustomTextField(
-                              title: "Mot de passe",
-                              placeholder: "••••••••••••••••",
-                              error: passwordError,
-                              controller: passwordController,
-                              disabled: isDisabled,
-                              keyboardType: TextInputType.visiblePassword,
-                              onChanged: (_) => setState(() {}),
-                            ),
+                              CustomTextField(
+                                title: "Mot de passe",
+                                placeholder: "••••••••••••••••",
+                                error: passwordError,
+                                controller: passwordController,
+                                disabled: isDisabled,
+                                keyboardType: TextInputType.visiblePassword,
+                                onChanged: (_) => setState(() {}),
+                              ),
 
-                            const SizedBox(height: 36),
+                              const SizedBox(height: 36),
 
-                            CupertinoButton.filled(
-                              padding: const EdgeInsets.symmetric(vertical: 12),
-                              minimumSize: Size.zero,
-                              onPressed:
-                                  (!fieldsAreEmpty && !isDisabled)
-                                      ? tryToLogin
-                                      : null,
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.center,
+                              CupertinoButton.filled(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 12,
+                                ),
+                                minimumSize: Size.zero,
+                                onPressed:
+                                    (!fieldsAreEmpty && !isDisabled)
+                                        ? tryToLogin
+                                        : null,
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    isDisabled
+                                        ? const CupertinoActivityIndicator()
+                                        : const Text("Accéder"),
+                                  ],
+                                ),
+                              ),
+
+                              const SizedBox(height: 30),
+
+                              Row(
                                 children: [
-                                  isDisabled
-                                      ? const CupertinoActivityIndicator()
-                                      : const Text("Accéder"),
+                                  Expanded(
+                                    child: Divider(
+                                      color: CupertinoColors.systemGrey
+                                          .withOpacity(.25),
+                                      indent: 30,
+                                      endIndent: 10,
+                                    ),
+                                  ),
+                                  const Text(
+                                    "ou",
+                                    style: TextStyle(
+                                      color: CupertinoColors.systemGrey,
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Divider(
+                                      color: CupertinoColors.systemGrey
+                                          .withOpacity(.25),
+                                      indent: 10,
+                                      endIndent: 30,
+                                    ),
+                                  ),
                                 ],
                               ),
-                            ),
 
-                            const SizedBox(height: 30),
+                              const SizedBox(height: 30),
 
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Divider(
-                                    color: CupertinoColors.systemGrey
-                                        .withOpacity(.25),
-                                    indent: 30,
-                                    endIndent: 10,
-                                  ),
+                              CupertinoButton.filled(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 12,
                                 ),
-                                const Text(
-                                  "ou",
+                                onPressed:
+                                    isDisabled
+                                        ? null
+                                        : () {
+                                          Navigator.of(context).push(
+                                            CupertinoPageRoute(
+                                              builder:
+                                                  (context) =>
+                                                      const RegistrationPage(),
+                                            ),
+                                          );
+                                        },
+                                child: const Text(
+                                  "Créer un compte",
                                   style: TextStyle(
-                                    color: CupertinoColors.systemGrey,
-                                    fontSize: 12,
+                                    color: CupertinoColors.white,
                                   ),
                                 ),
-                                Expanded(
-                                  child: Divider(
-                                    color: CupertinoColors.systemGrey
-                                        .withOpacity(.25),
-                                    indent: 10,
-                                    endIndent: 30,
-                                  ),
-                                ),
-                              ],
-                            ),
-
-                            const SizedBox(height: 30),
-
-                            CupertinoButton.filled(
-                              padding: const EdgeInsets.symmetric(vertical: 12),
-                              onPressed:
-                                  isDisabled
-                                      ? null
-                                      : () {
-                                        Navigator.of(context).push(
-                                          CupertinoPageRoute(
-                                            builder:
-                                                (context) =>
-                                                    const RegistrationPage(),
-                                          ),
-                                        );
-                                      },
-                              child: const Text(
-                                "Créer un compte",
-                                style: TextStyle(color: CupertinoColors.white),
                               ),
-                            ),
-                          ],
-                        ),
+                            ],
+                          ),
+                    ),
                   ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
+      
     );
   }
 
