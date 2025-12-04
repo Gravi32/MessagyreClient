2eb1d5f9f986951aefe6b5b4907383c3feb89d57
Gravi32 <gravina.pietro@gmail.com>
2025-12-04 02:00:34 +0100

    refactor: adapted animation to different screen sizes

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -204,10 +204,8 @@ class SearchPageState extends State<SearchPage> {
                             const SizedBox(height: 43),
                           ],
                         ),
-                        Positioned(
-                          top: MediaQuery.heightOf(context) - 350 * Curves.easeOutCubic.transform(alpha),
-                          right: 0,
-                          left: 0,
+                        Align(
+                          alignment: Alignment(0, 5 - 2.25 * Curves.easeOutCubic.transform(alpha)),
                           child: Image.asset("assets/messagyreGuy.png", errorBuilder: (_, _, _) => Image.asset("assets/MessagyreGuy.png")),
                         ),
                       ],
