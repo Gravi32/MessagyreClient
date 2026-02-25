ba2402a962dcb7e20dfe3fd4c0f6345d1de448ea
Gravi32 <gravina.pietro@gmail.com>
2026-02-25 15:49:28 +0100

    fix: fixed subject badges outline color being too dark

--- lib/utility/widgets/subject_badge.dart
+++ lib/utility/widgets/subject_badge.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:messagyre_client/configuration/app_colors.dart';
 import 'package:messagyre_client/database/models/subjects/subject.dart';
-import 'package:messagyre_client/utility/utility.dart';
 
 class SubjectBadge extends StatelessWidget {
   final Subject subject;
@@ -16,7 +15,7 @@ class SubjectBadge extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           color: subject.color,
-          border: Border.all(color: subject.color.withBrightness(-.15)),
+          border: Border.all(color: subject.color.withAlpha(100), strokeAlign: BorderSide.strokeAlignOutside, width: 1.5),
           borderRadius: BorderRadius.circular(size / 3.6),
         ),
         child: Center(child: Icon(subject.icon, color: AppColors.white, size: size * (1 - size / 70))),
