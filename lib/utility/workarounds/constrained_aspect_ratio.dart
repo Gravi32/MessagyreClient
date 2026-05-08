c84ace3ea11542611cdf9965a1d799bc84dbe0a8
Gravi32 <gravina.pietro@gmail.com>
2026-05-08 12:29:27 +0200

    refactor: constrained aspect ratio now handles different main axis

--- lib/utility/workarounds/constrained_aspect_ratio.dart
+++ lib/utility/workarounds/constrained_aspect_ratio.dart
@@ -2,19 +2,21 @@ import 'package:flutter/material.dart';
 
 class ConstrainedAspectRatio extends StatelessWidget {
   final Widget child;
-  final double maxAspectRatio;
+  final double? maxAspectRatio;
+  final Axis mainAxis;
 
-  const ConstrainedAspectRatio({super.key, required this.child, this.maxAspectRatio = 1.5});
+  const ConstrainedAspectRatio({super.key, required this.child, this.maxAspectRatio, this.mainAxis = .horizontal});
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        final double maxWidth = constraints.maxWidth;
-        final double maxHeightLimit = maxWidth * maxAspectRatio;
-
+        final Size limit = Size(constraints.maxHeight, constraints.maxWidth) * (maxAspectRatio ?? 1);
         return Container(
-          constraints: BoxConstraints(maxHeight: maxHeightLimit),
+          constraints: BoxConstraints(
+            maxHeight: mainAxis == .horizontal ? limit.height : double.infinity,
+            maxWidth: mainAxis == .vertical ? limit.width : double.infinity,
+          ),
           child: child,
         );
       },
